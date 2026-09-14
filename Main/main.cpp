509f7069a14986b9a85b645b7677b6287b306a8d
Jose Angel <joseangel.alzate@udea.edu.co>
2026-09-14 11:41:34 -0500

    calculo de bytes necesarios para el tablero

--- Main/main.cpp
+++ Main/main.cpp
@@ -5,9 +5,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello Luna! C:" << endl;
-    cout << "Hello Jose ;0" << endl;
-    //SWEET CRUSH
-    //copia de candy crush
+
+    int filas, columnas, cantidad_bytes;
+
+    cout<<"ingrese el numero de filas"<<endl;
+    cin>>filas;
+    cout<<"ingrese el numero de columnas"<<endl;
+    cin>>columnas;
+    unsigned char* cintabytes;
+    //Puntero que guardará la dirección de memoria de la cinta
+
+    cantidad_bytes = (filas * columnas * 3 + 7) / 8;
+    //posible funcion // Calculo de cuantos bytes necesitamos para guardar todas las fichas
+
+    cintabytes = new unsigned char[cantidad_bytes]();
+    //Reserva en memoria el espacio necesario para la cinta e inicializa los bytes en 0
+
+    delete[] cintabytes ;    //liberamos la memoria reservada para la cinta
     return 0;
 }
