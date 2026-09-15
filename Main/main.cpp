c9fd10df8c05a484df93a68081765a1f98314379
Jose Angel <joseangel.alzate@udea.edu.co>
2026-09-15 00:31:53 -0500

    desplazamiento de los bits

--- Main/main.cpp
+++ Main/main.cpp
@@ -12,6 +12,17 @@ int main()
     cin>>filas;
     cout<<"ingrese el numero de columnas"<<endl;
     cin>>columnas;
+
+    // Fichas:
+    // 000 = libre
+    // 001 = ficha 1
+    // 010 = ficha 2
+    // 011 = ficha 3
+    // 100 = ficha 4
+    // 101 = ficha 5
+    // 110 = ficha 6
+    // 111 = especial
+
     unsigned char* cintabytes;
     //Puntero que guardará la dirección de memoria de la cinta
 
@@ -20,6 +31,20 @@ int main()
 
     cintabytes = new unsigned char[cantidad_bytes]();
     //Reserva en memoria el espacio necesario para la cinta e inicializa los bytes en 0
+
+    cintabytes[0] = 0b001<<5;
+    for (int i = 7; i >= 0; i--)
+    {
+        cout << ((cintabytes[0] >> i) & 1);
+    }
+    cout << endl;
+
+    cintabytes[0] |= 0b111<<2;
+    for (int i = 7; i >= 0; i--)
+    {
+        cout << ((cintabytes[0] >> i) & 1);
+    }
+    cout << endl;
 
     delete[] cintabytes ;    //liberamos la memoria reservada para la cinta
     return 0;
